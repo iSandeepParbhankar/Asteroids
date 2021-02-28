0145917dc98ac1469e140b9d555fe2f30e00bbd3
Sandeep Parbhankar <sandeep.parbhankar@neosoftmail.com>
2021-03-01 00:11:11 +0530

    Test case created

--- app/src/androidTest/java/com/udacity/asteroidradar/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/udacity/asteroidradar/ExampleInstrumentedTest.kt
@@ -1,12 +1,19 @@
 package com.udacity.asteroidradar
 
+import androidx.room.Room
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.udacity.asteroidradar.database.AsteroidDao
+import com.udacity.asteroidradar.database.AsteroidDatabase
+import com.udacity.asteroidradar.utils.DateUtils.startDate
+import org.junit.After
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Before
+import java.io.IOException
 import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.collections.ArrayList
@@ -23,5 +30,34 @@ class ExampleInstrumentedTest {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.udacity.asteroidradar", appContext.packageName)
+    }
+
+
+    private lateinit var asteroidDao: AsteroidDao
+    private lateinit var db: AsteroidDatabase
+
+    @Before
+    fun createDb() {
+        val context = InstrumentationRegistry.getInstrumentation().targetContext
+        // Using an in-memory database because the information stored here disappears when the
+        // process is killed.
+        db = Room.inMemoryDatabaseBuilder(context, AsteroidDatabase::class.java)
+                // Allowing main thread queries, just for testing.
+                .allowMainThreadQueries()
+                .build()
+        asteroidDao = db.asteroidDao
+    }
+
+    @After
+    @Throws(IOException::class)
+    fun closeDb() {
+        db.close()
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun getAllRecords() {
+        val list = asteroidDao.getAsteroids(startDate())
+        assertNotEquals("Size: ${list.value?.size}", list.value?.size, 0)
     }
 }
